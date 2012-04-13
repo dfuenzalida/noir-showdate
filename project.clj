59b3d275318084bf20e10094c3f782051e3d794b
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-04-13 20:41:25 -0300

    Agregado plugin lein-war

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
+            :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.13"]]
             :main showdate.server)
 
