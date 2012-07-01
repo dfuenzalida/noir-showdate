de4c845c966996c0aa067f091ad66f1c8ff7fcc6
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-07-01 00:31:17 -0400

    Usando nuevo modelo para listar clientes

--- src/showdate/views/welcome.clj
+++ src/showdate/views/welcome.clj
@@ -1,11 +1,26 @@
 (ns showdate.views.welcome
-  (import java.util.Date)
-  (:require [showdate.views.common :as common])
+  (:require [showdate.views.common :as common]
+            [showdate.models.customers :as customers])
 
-  (:use [noir.core :only [defpage]]
+  (:use [noir.core]
         [hiccup.core :only [html]]))
 
+;; notar destructuring aqui
+(defpartial table-rows [{:keys [id name]}]
+  [:tr
+   [:td id ][:td name]]
+  )
+
+(defpartial show-item [item]
+  [:li (str item)])
+
 (defpage "/" []
-         (common/layout
-          [:h1 (str "La hora: " (Date.))]
-          ))
+  (common/layout
+   [:h1 {:style "padding: 10px 0px 10px"} "Lista de Clientes"]
+
+   [:div.row
+    [:table {:class "table table-bordered table-stripped"}
+     (map table-rows (customers/get-customers))]
+    ]
+
+   ))
