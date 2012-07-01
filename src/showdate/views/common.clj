50ba4633b452b3bf4a80477ae9d347dc69795b31
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-07-01 00:30:45 -0400

    Actualizado template, usando Twitter bootstrap

--- src/showdate/views/common.clj
+++ src/showdate/views/common.clj
@@ -1,12 +1,22 @@
 (ns showdate.views.common
+  (import java.util.Date)
   (:use [noir.core :only [defpartial]]
         [hiccup.page-helpers :only [include-css html5]]))
 
 (defpartial layout [& content]
-            (html5
-              [:head
-               [:title "showdate"]
-               (include-css "/css/no-reset.css")]
-              [:body
-               [:div#wrapper
-                content]]))
+  (html5
+   [:head
+    [:title "Sample"]
+    (include-css
+     "http://twitter.github.com/bootstrap/assets/css/bootstrap.css")
+    (include-css
+     "http://twitter.github.com/bootstrap/assets/css/bootstrap-responsive.css")]
+   [:body
+    [:div.container
+     [:div content]
+     [:hr]
+     [:footer.footer
+      [:p.pull-right [:a {:href "#"} "Inicio"]]
+      [:p (str "Generado: " (Date.))]]
+     ]]
+   ))
