47176b3021fe8057f1e1e9f86d1006ea86f37532
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-07-04 22:54:58 -0400

    Updated libraries and plugin versions

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
                            [noir "1.2.1"]
                            [korma "0.3.0-beta9"]
                            [mysql/mysql-connector-java "5.1.6"]
-                           [log4j "1.2.15"
+                           [log4j "1.2.17"
                             :exclusions [javax.mail/mail
                                          javax.jms/jms
                                          com.sun.jdmk/jmxtools
                                          com.sun.jmx/jmxri]]]
-            :plugins [[lein-ring "0.6.4"]]
+            :plugins [[lein-ring "0.7.1"]]
             :ring {:handler showdate.server/handler}
             :main showdate.server)
 
