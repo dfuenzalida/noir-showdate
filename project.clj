4a9e6537d06b25765fc901561a248881eb5e950e
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-09-26 18:43:28 -0300

    removed init function from startup, upgraded lein ring, etc.

--- project.clj
+++ project.clj
@@ -4,14 +4,19 @@
                            [noir "1.2.1"]
                            [korma "0.3.0-beta9"]
                            [mysql/mysql-connector-java "5.1.6"]
+
+                           ;; required by korma:
                            [log4j "1.2.17"
                             :exclusions [javax.mail/mail
                                          javax.jms/jms
                                          com.sun.jdmk/jmxtools
                                          com.sun.jmx/jmxri]]]
-            :plugins [[lein-ring "0.7.1"]]
-            :ring {:handler showdate.server/handler
-                   ;; initialization code which runs once
-                   :init showdate.server/init}
+            :plugins [[lein-ring "0.7.5"]]
+            :ring {
+                   :handler showdate.server/handler
+
+                   ;; initialization code which runs once here:
+                   ;; :init showdate.server/init
+                   }
             :main showdate.server)
 
